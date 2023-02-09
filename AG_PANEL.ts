1e874cd0ce2cac192d2c3dd321659616741125f5
谭期元 <644669785@qq.com>
2023-02-09 23:27:08 +0800

    增加前言｜开始界面

--- AG_PANEL.ts
+++ AG_PANEL.ts
@@ -36,12 +36,14 @@
       mountElement: HTMLElement,
       position: "top" | "bottom" | "insert",
       insertBefore?: HTMLElement,
+      append?: boolean,
     ): boolean;
 
     protected abstract addToElements(
       mountElement: HTMLElement,
       position: "top" | "bottom" | "insert",
       insertBefore?: HTMLElement,
+      append?: boolean,
       ...element: Array<HTMLElement>
     ): boolean;
 
@@ -70,8 +72,8 @@
 
     protected getElement(
       tagName: string,
-      tagId?: string,
       tagClass?: string,
+      tagId?: string,
     ): HTMLElement {
       const tag = document.createElement(tagName);
       if (tagId) tag.setAttribute("id", tagId);
@@ -88,8 +90,11 @@
       const keys = Object.keys(attributes);
       if (!element || !keys.length) return false;
       const storage = ["textContent", "innerHTML", "innerText"];
+      const selectorStorage = ["id", "class", "name", "agActive"];
       for (const key of keys) {
         if (storage.includes(key)) element.innerHTML = attributes[key];
+        else if (selectorStorage.includes(key))
+          element.setAttribute(key, attributes[key]);
         else if (Object.prototype.hasOwnProperty.call(element.style, key)) {
           element.style.setProperty(
             key.replace(/([A-Z])/g, "-$1").toLowerCase(),
@@ -125,14 +130,16 @@
       mountElement: HTMLElement,
       position: "top" | "bottom" | "insert" = "top",
       insertBefore?: HTMLElement,
+      append: boolean = false,
     ): boolean {
       if (!element || !mountElement) return false;
-
-      if (element.id) document.querySelector(`#${element.id}`)?.remove();
-      else if (element.className)
-        document.querySelector(`.${element.className}`)?.remove();
-      else if (mountElement.contains(element))
-        mountElement.removeChild(element);
+      if (!append)
+        if (element.id) document.querySelector(`#${element.id}`)?.remove();
+        else if (element.className) {
+          const elem = document.querySelectorAll(`.${element.className}`);
+          for (const item of elem) item.remove();
+        } else if (mountElement.contains(element))
+          mountElement.removeChild(element);
 
       switch (position) {
         case "top":
@@ -153,10 +160,11 @@
       mountElement: HTMLElement,
       position: "top" | "bottom" | "insert" = "top",
       insertBefore?: HTMLElement,
+      append?: boolean,
       ...element: Array<HTMLElement>
     ): boolean {
       for (const item of element) {
-        this.addToElement(item, mountElement, position, insertBefore);
+        this.addToElement(item, mountElement, position, insertBefore, append);
       }
       return true;
     }
@@ -188,6 +196,16 @@
       super();
       this.createGlobalErrorHandler();
 
+      const style = this.getElement("style");
+      this.setElementStyleOrText(style, {
+        innerHTML: `
+        li.ag-options[agactive="true"] { color:orange;  }
+        li.ag-options:hover { color:orange; }
+        li.ag-options { color:#999999; }
+        `,
+      });
+      this.addToElement(style, document.head, "bottom");
+
       const panel = this.getElement("div", panelName);
       this.panel = panel;
       this.setElementStyleOrText(panel, {
@@ -204,6 +222,7 @@
         color: "#999999",
         borderRadius: "0 5px 5px 0",
       });
+      let draw: HTMLElement;
 
       const columnLeft = this.getElement("div");
       this.setElementStyleOrText(columnLeft, {
@@ -272,28 +291,108 @@
           columnLeft,
           "bottom",
           undefined,
+          false,
           rowOne,
           rowTwo,
           rowThree,
           rowFour,
         );
 
-        const options: Array<{ label: string; index: number }> = [
+        const options: Array<{ label: string; event: Function }> = [
           {
             label: "前言",
-            index: 0,
+            event: () => {
+              console.log("前言 begin");
+              const ulItem = this.getElement("ul", "ag-draw");
+              this.setElementStyleOrText(ulItem, {
+                paddingLeft: "20px",
+                cursor: "default",
+                overflowX: "hidden",
+                margin: "0",
+              });
+              const messages = [
+                {
+                  textContent:
+                    "本工具为个人制作，仅供交流学习使用，请不要用于商业传播，否则后果自负！使用本工具即代表您同意本条款。",
+                  color: "",
+                  backgroundColor: "",
+                },
+                {
+                  textContent:
+                    "工具的初衷是为了让使用者释放双手，解决方案并未包含入侵的攻击和技术，仅模拟人工打开相应的任务进行操作，对程序本身不会造成任何影响，如果对平台技术层面有影响请转告我们。",
+                  color: "",
+                  backgroundColor: "",
+                },
+                {
+                  textContent:
+                    "使用者在使用本软件前已经得知可能涉嫌《非法入侵计算机信息系统罪》，但滥用本软件造成的—切后果自行承担!",
+                  color: "",
+                  backgroundColor: "",
+                },
+                {
+                  textContent:
+                    "使用方法：点击【开始任务】即可自动完成电脑端每日任务（除【登录】任务），期间可以切到后台，但不要最小化，中断后可以点击【开始/继续任务】即可继续任务。",
+                  color: "white",
+                  backgroundColor: "orange",
+                },
+              ];
+              for (const item of messages) {
+                const liItem = this.getElement("li");
+                const { textContent, color, backgroundColor } = item;
+                this.setElementStyleOrText(liItem, {
+                  textContent,
+                  color,
+                  backgroundColor,
+                  margin: "5px 0",
+                });
+                this.addToElement(liItem, ulItem, "bottom");
+              }
+              this.addToElement(ulItem, draw, "bottom");
+              console.log("前言 end");
+            },
           },
           {
             label: "开始",
-            index: 1,
+            event: () => {
+              console.log("开始...");
+              const ulItem = this.getElement("ul", "ag-draw");
+              this.setElementStyleOrText(ulItem, {
+                paddingLeft: "0",
+                cursor: "default",
+                overflowX: "hidden",
+                color: "orange",
+                margin: "0",
+                marginBottom: "15px",
+                height: "362px",
+                width: "100%",
+                backgroundColor: "#1e1e1e",
+                borderRadius: "10px",
+              });
+              const buttonItem = this.getElement("button", "ag-draw");
+              this.setElementStyleOrText(buttonItem, {
+                textContent: "开始任务",
+                backgroundColor: "#e22b2b00",
+                color: "orange",
+                border: "1px solid orange",
+                borderRadius: "10px",
+                height: "28px",
+                fontSize: "14px",
+                margin: "0 auto",
+                padding: "0 70px",
+                width: "auto",
+                cursor: "pointer",
+              });
+              this.addToElement(ulItem, draw, "bottom");
+              this.addToElement(buttonItem, draw, "bottom", undefined, true);
+            },
           },
           {
             label: "配置",
-            index: 2,
+            event: () => {},
           },
           {
             label: "捐助",
-            index: 3,
+            event: () => {},
           },
         ];
         const menu = this.getElement("ul");
@@ -306,24 +405,26 @@
         });
         this.addToElement(menu, columnLeft, "bottom");
         for (const item of options) {
-          const li = this.getElement("li");
+          const li = this.getElement("li", "ag-options");
           this.setElementStyleOrText(li, {
             textContent: item.label,
             cursor: "pointer",
             height: "40px",
             lineHeight: "40px",
-            color: "#999999",
             fontSize: "16px",
           });
-          li.onmouseover = () => {
+          li.onclick = () => {
+            item.event();
+            const ele = document.querySelector("[agActive=true]");
+            if (ele && ele instanceof HTMLElement) {
+              this.setElementStyleOrText(ele, {
+                agActive: "false",
+              });
+            }
             this.setElementStyleOrText(li, {
-              color: "orange",
+              agActive: "true",
             });
-          };
-          li.onmouseleave = () => {
-            this.setElementStyleOrText(li, {
-              color: "#999999",
-            });
+            console.log("完成...");
           };
           this.addToElement(li, menu, "bottom");
         }
@@ -341,48 +442,27 @@
         this.setElementStyleOrText(rowOne, {
           textContent: "爱果学习强国　v23.2.X",
           height: "30px",
+          lineHeight: "30px",
           textAlign: "center",
         });
 
         const rowTwo = this.getElement("div");
+        draw = rowTwo;
         this.setElementStyleOrText(rowTwo, {
           height: "420px",
           display: "flex",
           padding: "0 15px",
+          flexDirection: "column",
         });
 
-        const rowTwoItem = this.getElement("ul");
-        this.setElementStyleOrText(rowTwoItem, {
-          paddingLeft: "15px",
-          cursor: "default",
-        });
-        const messages = [
-          {
-            textContent:
-              "本工具为个人制作，仅供交流学习使用，请不要用于商业传播，否则后果自负！使用本工具即代表您同意本条款。",
-            color: "",
-            backgroundColor: "",
-          },
-          {
-            textContent:
-              "使用方法：点击【开始任务】即可自动完成电脑端每日任务（除【登录】任务），期间可以切到后台，但不要最小化，中断后可以点击【开始/继续任务】即可继续任务。",
-            color: "white",
-            backgroundColor: "orange",
-          },
-        ];
-        for (const item of messages) {
-          const liItem = this.getElement("li");
-          const { textContent, color, backgroundColor } = item;
-          this.setElementStyleOrText(liItem, {
-            textContent,
-            color,
-            backgroundColor,
-          });
-          this.addToElement(liItem, rowTwoItem, "bottom");
-        }
-
-        this.addToElement(rowTwoItem, rowTwo, "bottom");
-        this.addToElements(columnCenter, "bottom", undefined, rowOne, rowTwo);
+        this.addToElements(
+          columnCenter,
+          "bottom",
+          undefined,
+          false,
+          rowOne,
+          rowTwo,
+        );
       }
 
       const columnRight = this.getElement("div");
@@ -431,6 +511,7 @@
         panel,
         "bottom",
         undefined,
+        false,
         columnLeft,
         columnCenter,
         columnRight,
